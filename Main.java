9fdbbf4d3c9a0fdf763e1e0285f74e7bdfbc6ccf
i-am-neegr <martial.artem@gmail.com>
2024-10-23 20:03:05 +0300

    first attempts

--- Main.java
+++ Main.java
@@ -3,12 +3,59 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog - " + dog);
+        System.out.println("cat - " + cat);
+        System.out.println("paper - " + paper);
+
+        System.out.println();
+
+        dog += 4;
+        cat += 4;
+        paper += 4;
+        System.out.println("dog - " + dog);
+        System.out.println("cat - " + cat);
+        System.out.println("paper - " + paper);
+
+        System.out.println();
+
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println("dog - " + dog);
+        System.out.println("cat - " + cat);
+        System.out.println("paper - " + paper);
+
+        System.out.println();
+
+        var friend = 19;
+        System.out.println("friend - " + friend);
+        friend *= 2;
+        System.out.println("friend - " + friend);
+        friend /= 7;
+        System.out.println("friend - " + friend);
+
+        System.out.println();
+
+        var frog = 3.5;
+        System.out.println("frog - " + frog);
+        frog *= 10;
+        System.out.println("frog - " + frog);
+        frog /= 3.5;
+        System.out.println("frog - " + frog);
+        frog += 4;
+        System.out.println("frog - " + frog);
+
+        System.out.println();
+
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+
+        System.out.println(firstBoxerWeight + secondBoxerWeight);
+
     }
 }
